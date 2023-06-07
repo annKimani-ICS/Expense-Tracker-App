ea62ea710c9f7f8c804c9cb738b90521a0b0366a
annkimani <ann.wangari@strathmore.edu>
2023-06-07 19:48:08 +0300

    Backend changes to the routes

--- backend/routes/app.js
+++ backend/routes/app.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const PORT = process.env.PORT 
 
 const server = () => {
-    console.log('You are listening to port:' , PORT);
+    console.log('You are listening to port no:' , PORT);
 }
 
 server()
